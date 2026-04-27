0264c3ff0ab1e399787109e0ce415055225daaff
Dat Mai <hiendat04@gmail.com>
2026-04-27 14:44:48 -0500

    feat: enhance circuit breaker configuration and add response time header for loan requests

--- src/main/java/com/microservice/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/microservice/gatewayserver/GatewayserverApplication.java
@@ -1,11 +1,18 @@
 package com.microservice.gatewayserver;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -25,18 +32,31 @@ public class GatewayserverApplication {
                                 .circuitBreaker(c -> c.setName("accountsCircuitBreaker")
                                         .setFallbackUri("forward:/contact-support")))
                         .uri("lb://ACCOUNTS"))
+                .route(r -> r.path("/banking/loans/**")
+                        .filters(f -> f
+                                .rewritePath("/banking/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .retry(c -> c.setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
+                        .uri("lb://LOANS"))
                 .route(r -> r.path("/banking/cards/**")
                         .filters(f -> f
                                 .rewritePath("/banking/cards/(?<segment>.*)", "/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://CARDS"))
-                .route(r -> r.path("/banking/loans/**")
-                        .filters(f -> f
-                                .rewritePath("/banking/loans/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-                        .uri("lb://LOANS"))
                 .build();
 
     }
 
+    // Change the default time limiter of circuit breaker
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(
+                id -> new Resilience4JConfigBuilder(id)
+                        .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                        .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
+                        .build()
+        );
+    }
 }
