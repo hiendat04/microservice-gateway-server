aca5ae4cb9326eefcfe0036a10b0f0883c89bf7a
Dat Mai <hiendat04@gmail.com>
2026-05-08 18:25:14 -0500

    feat: implement Keycloak role converter for JWT authentication and update security configuration

--- src/main/java/com/microservice/gatewayserver/config/SecurityConfig.java
+++ src/main/java/com/microservice/gatewayserver/config/SecurityConfig.java
@@ -2,11 +2,16 @@ package com.microservice.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -14,13 +19,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange(exchange -> {
+        http.authorizeExchange(exchange ->
             exchange.pathMatchers(HttpMethod.GET).permitAll()
-                    .pathMatchers("/banking/accounts/**").authenticated()
-                    .pathMatchers("/banking/cards/**").authenticated()
-                    .pathMatchers("/banking/loans/**").authenticated();
-        }).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                    .pathMatchers("/banking/accounts/**").hasRole("ACCOUNTS")
+                    .pathMatchers("/banking/cards/**").hasRole("CARDS")
+                    .pathMatchers("/banking/loans/**").hasRole("LOANS")
+        ).oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
         http.csrf(ServerHttpSecurity.CsrfSpec::disable);
         return http.build();
     }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+
 }
